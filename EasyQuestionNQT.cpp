221a69e14e0317b65ff7071dd2cc486f5288ce17
Ashoka Bhat <164073554+ASHOKABHAT69@users.noreply.github.com>
2026-06-28 22:36:33 +0530

    Implement Fibonacci series printing with error check
    Add a description for printing Fibonacci series with error handling.

---
+++ EasyQuestionNQT.cpp
@@ -0,0 +1,9 @@
+Print Fibonacci sereis till a given number N. 
+By deafult it should start form these values,
+s[0]=5 
+s[1]=6 
+if N<0 print "Error: N less than zero" without double quotes.
+
+A--N--S;
+How can you solve
+you can use an array and or you can use two varibales and calculate sum and then print anything works!!
